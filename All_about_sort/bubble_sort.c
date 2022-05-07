4c8131506e296e74784322b91f438cb8db1ca649
Arteiimis <lionelym0987@gmail.com>
2022-05-08 01:05:08 +0800

    5.8

--- All_about_sort/bubble_sort.c
+++ All_about_sort/bubble_sort.c
@@ -1,22 +1,25 @@
 #include<stdio.h>
-void bubble_sort (int *arr, int len);
+#define MAX_SIZE 100
+
+void bubble_sort (int *arr);
 void swap( int *a, int *b );
+int listlen(int *arr);
 
-int main(){
-    int arr[] = { 22, 34, 3, 32, 82, 55, 89, 50, 37, 5, 64, 35, 9, 70 };
-    int len = sizeof(arr) / sizeof(arr[0]);
-    int sigh = 0;
+// int main(){
+//     int arr[MAX_SIZE] = {22, 34, 3, 32, 82, 55, 89, 50, 37, 5, 64, 35, 9, 70};
+//     int sigh = 0;
     
-    bubble_sort(arr, len);
-    for( sigh=0; sigh<len; sigh++ )
-        printf("%d ", arr[sigh]);
+//     bubble_sort(arr);
+//     printf("%d\n", listlen(arr));
+//     for( sigh=0; sigh<listlen(arr); sigh++ )
+//         printf("%d ", arr[sigh]);
     
-    return 0;
-}
+//     return 0;
+// }
 
-void bubble_sort (int *arr, int len) {
+void bubble_sort (int *arr) {
     int i, j;    //i：遍历整个数组，j:作为前后比较的标志。
-    
+    int len = listlen(arr);
     for ( i=0; i < len - 1; i++ ) {
         int flag = 0;       //优化标志，每次循环判断是否交换。
         for ( j=0; j < len - 1 - i; j++ ) {
@@ -34,4 +37,11 @@ void swap( int *a, int *b ){
     int temp = *a;
     *a = *b;
     *b = temp;
+}
+
+int listlen(int *arr) {
+    int len = 0;
+    while (arr[len] != 0)
+        len++;
+    return len;
 }
