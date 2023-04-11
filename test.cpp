1fb61425b16bbd12a55dfe4a4150bfb4b6d5ab82
Arteiimis <lionelym0987@gmail.com>
2023-04-11 13:39:36 +0800

    23_4_11

--- test.cpp
+++ test.cpp
@@ -48,6 +48,48 @@ void mergeSort(vector<int> &nums)
     mergeSort(nums, 0, nums.size() - 1);
 }
 
+// 归并排序迭代实现
+void mergeSort_ite(vector<int>& nums) {
+    int n = nums.size();
+    // 子数组的大小从 1 开始，每次翻倍, 作为外层最大循环, 在这个大循环结束之后, 数组容量才会翻倍.
+    for (int size = 1; size < n; size *= 2) 
+    {
+        // left 表示左子数组的起始位置
+        for (int left = 0; left < n - size; left += 2 * size) 
+        {
+            int mid = left + size - 1;  // 计算中间位置
+            int right = min(left + 2 * size - 1, n - 1);  // 计算右子数组的结束位置
+            vector<int> temp(right - left + 1);  // 临时数组
+            int i = left, j = mid + 1, k = 0;  // i、j、k 分别指向左、右、临时数组的下标
+            while (i <= mid && j <= right)   // 左右子数组都有元素
+            {
+                if (nums[i] <= nums[j])     // 如果左子数组的元素小于等于右子数组的元素
+                {  
+                    temp[k++] = nums[i++];  // 将左子数组的元素放入临时数组中
+                } else                      // 如果左子数组的元素大于右子数组的元素
+                {  
+                    temp[k++] = nums[j++];  // 将右子数组的元素放入临时数组中
+                }
+            }
+            // 将左子数组中剩余的元素放入临时数组中
+            while (i <= mid) 
+            {
+                temp[k++] = nums[i++];
+            }
+            // 将右子数组中剩余的元素放入临时数组中
+            while (j <= right) 
+            {
+                temp[k++] = nums[j++];
+            }
+            // 将临时数组中的元素复制回原数组
+            for (int p = 0; p < k; p++) 
+            {
+                nums[left + p] = temp[p];
+            }
+        }
+    }
+}
+
 int main()
 {
     mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
@@ -58,7 +100,7 @@ int main()
     }
 
     auto start = chrono::steady_clock::now();
-    mergeSort(nums);
+    mergeSort_ite(nums);
     auto end = chrono::steady_clock::now();
     
     auto duration = chrono::duration_cast<chrono::microseconds>(end - start);
